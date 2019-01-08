9663329176aa9bdfd4db575f4e271b97b2507d98
iosc <boyd.chung@gmail.com>
2019-01-08 14:53:20 +0800

    Create config_local.php

---
+++ config_local.php
@@ -0,0 +1,11 @@
+<?
+define('COMPANY_NAME', 'Online Reservation');
+define('SITEDOMAIN', 'http://localhost/www.vdo.my/ihom/');
+define('SITEURL', 'http://localhost/www.vdo.my/ihom/');
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'hotel_booking_form');
+define('CHARSET', 'utf8');
+?>
