2fce026f6a8621c81d427f02b12fc05debbb2f18
iosc <boyd.chung@gmail.com>
2019-01-08 14:53:07 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+define('COMPANY_NAME', 'Online Reservation');
+define('SITEDOMAIN', 'http://vdo.my/ihom/');
+define('SITEURL', 'http://vdo.my/ihom/');
+
+define('HOST', 'localhost');
+define('USER', 'vdomy_ihom');
+define('PASSWORD', '');
+define('DATABASE', 'vdomy_ihom');
+define('CHARSET', 'utf8');
+?>
