fe9fe6c64f1c8228e10e4b7abcd1ac63ecca0de6
elson chow <cht8987@gmail.com>
2019-01-08 15:58:15 +0800

    Update config_local.php

--- config_local.php
+++ config_local.php
@@ -1,11 +1,11 @@
 <?
-define('COMPANY_NAME', 'Online Booking App');
+define('COMPANY_NAME', 'Online Booking App -Elson');
 define('SITEDOMAIN', 'http://localhost/BookingApp');
 define('SITEURL', 'http://localhost/BookingApp');
 
 define('HOST', 'localhost');
 define('USER', 'root');
-define('PASSWORD', '');
+define('PASSWORD', '123456');
 define('DATABASE', 'bookingapp');
 define('CHARSET', 'utf8');
 ?>
