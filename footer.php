a1244d30241d7daa3eb4694f81645a2b78295863
iosc <boyd.chung@gmail.com>
2019-01-08 14:53:28 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <footer>
+    <hr />
+    <p class="navbar navbar-expand-lg navbar-light bg-light">Copyright
+	<?
+    	echo COMPANY_NAME;
+		echo ' 2018 to ', date('Y');
+	?></p>
+    </footer>
+</div><? //end for class container ?>    
+</body>
+</html>
