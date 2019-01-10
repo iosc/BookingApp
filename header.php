776265a9ef97a92db7619d065b4668f058151c87
iosc <boyd.chung@gmail.com>
2019-01-10 10:52:07 +0800

    Update header.php

--- header.php
+++ header.php
@@ -7,19 +7,19 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-    
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 	<script src="js/script.js?v=s<? echo date('is'); ?>" ></script>
-    
+
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css?v=s<? echo date('is'); ?>" />
 </head>
 
 <body>
-<?php /*?><pre>
+<pre>
 <div id="log_debug_div" style="position: fixed; top: 100px; right: 5px; border: 2px red solid; display: block; width: 350px; min-height: 150px; background-color: #FCF; padding: 5px; overflow: auto; z-index: 9999;"></div>
 </pre>
 
@@ -29,7 +29,7 @@ $( "#log_debug_div" ).draggable();
 $( "#log_debug_div" ).resizable();
 } );
 </script>
-<?php */?>  
+
 <div class="container-fluid">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -50,13 +50,13 @@ $( "#log_debug_div" ).resizable();
       <li class="nav-item">
           <a class="nav-link" href="#" onclick="readBookingRecords();">Bookings</a>
 </li>
-      
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">Customer</a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#add_new_record_modal">Add New Record</a>
           <?php /*?><a class="dropdown-item" href="#">Another action</a><?php */?>
-          
+
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
